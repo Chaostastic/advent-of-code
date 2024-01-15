8c9cf857e4b46ea82e7e994486a9efeb11fbc90c
Chaostastic <roodey72mc@gmail.com>
2024-01-15 13:51:48 +0200

    Refactor

--- 2023/day-01/trebuchet.c
+++ 2023/day-01/trebuchet.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
+#define MAX_LINE_LENGTH 50
 
-int part1(FILE *file) {
-    char ch;
-    int firstDigit = 0;
-    int lastDigit;
+int stringLength(char* string);
+int getDigit(char *string, int position);
+int getCalValue(char *line);
+
+int part1(char *filename) {
+    FILE *file = fopen(filename, "r");
+
+    char line[MAX_LINE_LENGTH];
     int sum = 0;
-    while ((ch = fgetc(file)) != EOF) {
-        if (ch >= '1' && ch <= '9') {
-            int digit = ch - '0';
-            if (firstDigit == 0) {
-                firstDigit = digit;
-            }
-            lastDigit = digit;
-        } else if (ch == '\n') {
-            sum += firstDigit * 10 + lastDigit;
-            firstDigit = 0;
-        }
+    while (fgets(line, MAX_LINE_LENGTH, file)) {
+        sum += getCalValue(line);
     }
-    sum += firstDigit * 10 + lastDigit;
     return sum;
+
+    fclose(file);
 }
 
 int main(void) {
-    FILE *file = fopen("input.txt", "r");
+    printf("Part 1: %d\n", part1("input.txt"));
+}
 
-    printf("Part 1: %d\n", part1(file));
+int stringLength(char* string) {
+    int i = 0;
+    while (string[i] != '\0') {
+        ++i;
+    }
+    return i;
+}
 
-    fclose(file);
+int getDigit(char *string, int position) {
+    char ch = string[position];
+    if (ch >= '1' && ch <= '9') {
+        return ch - '0';
+    }
+    return -1;
+}
+
+int getCalValue(char *line) {
+    int firstDigit = 0;
+    int lastDigit;
+    int length = stringLength(line);
+    for (int i = 0; i < length; ++i) {
+        int digit = getDigit(line,i);
+        if (digit == -1) continue;
+        if (firstDigit == 0) {
+            firstDigit = digit;
+        }
+        lastDigit = digit;
+    }
+    return firstDigit * 10 + lastDigit;
 }
